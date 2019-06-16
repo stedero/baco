a2d2038fb5939bc32dfbf4047b172a491ea2df6f
Steef de Rooi <steef@Bakvis.fritz.box>
2019-06-16 15:38:00 +0200

    more fields

--- model/ravorecord.go
+++ model/ravorecord.go
@@ -58,7 +58,39 @@ type RavoRecord struct {
 	ProductwijzigingInternAantal              RavoBool
 	AndersAantal                              RavoBool
 	AndersWat                                 string `xml:"AndersWat"`
+	VeiligheidsrisicoImpact                   string `xml:"VeiligheidsrisicoImpact"`
+	MilieuschadeRisicoImpact                  string `xml:"MilieuschadeRisicoImpact"`
+	KlantklachtImpact                         string `xml:"KlantklachtImpact"`
+	WettelijkVereistImpact                    string `xml:"WettelijkVereistImpact"`
+	GarantieAanspraakImpact                   string `xml:"GarantieAanspraakImpact"`
+	NietConformFunctieEisenImpact             string `xml:"NietConformFunctieEisenImpact"`
+	NietConformKwaliteitEisenImpact           string `xml:"NietConformKwaliteitEisenImpact"`
+	AndereLeverancier                         RavoBool
+	AndereLeverancier2                        RavoBool
+	VoorNadeel                                int64 `xml:"VoorNadeel"`
+	VoorNadeel2                               RavoBool
 	Probleemstelling                          RavoHTML
+	KlantklachtNr                             string `xml:"KlantklachtNr"`
+	NotificationNr                            string `xml:"NotificationNr"`
+	VoorstelVoorVerbetering                   RavoHTML
+	PrioriteitKlant                           string `xml:"PrioriteitKlant"`
+	PrioriteitAanvrager                       int64  `xml:"PrioriteitAanvrager"`
+	CommercielePrioriteit                     int64  `xml:"CommercielePrioriteit"`
+	MeerkostenGeschat                         RavoBool
+	MinderkostenGeschat                       int64  `xml:"MinderkostenGeschat"`
+	NotitiesRWVbehandelaarNaam                string `xml:"NotitiesRWVbehandelaarNaam"`
+	NotitiesRWVbehandelaar                    string `xml:"NotitiesRWVbehandelaar"`
+	BetrokkenEngineering                      string `xml:"BetrokkenEngineering"`
+	BetrokkenBedrijfsburo                     string `xml:"BetrokkenBedrijfsburo"`
+	BetrokkenInkoopLogistiek                  string `xml:"BetrokkenInkoopLogistiek"`
+	BetrokkenProductie                        string `xml:"BetrokkenProductie"`
+	BetrokkenSales                            string `xml:"BetrokkenSales"`
+	BetrokkenAfterSales                       string `xml:"BetrokkenAfterSales"`
+	BetrokkenTraining                         string `xml:"BetrokkenTraining"`
+	InschattingUren                           int64  `xml:"InschattingUren"`
+	Normuur                                   int64  `xml:"Normuur"`
+	InschattingEngTotaal                      int64  `xml:"InschattingEngTotaal"`
+	InschattingOverig                         int64  `xml:"InschattingOverig"`
 }
 
 // ReadRavoRecord read a Ravo XML record.
