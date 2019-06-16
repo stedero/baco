fdc085f1322779025093593d5ce1a91075bb27ce
Steef de Rooi <steef@Bakvis.fritz.box>
2019-06-16 13:26:42 +0200

    added  more fields

--- model/ravorecord.go
+++ model/ravorecord.go
@@ -7,15 +7,58 @@ import (
 	"log"
 )
 
+// RavoBool defines Ravo boolean type as encoded in XML
+type RavoBool struct {
+	Nil   bool `xml:"nil,attr" json:"nil"`
+	Value bool `xml:",innerxml" json:"value"`
+}
+
+type RavoHTML struct {
+	InnerXML string `xml:",innerxml"`
+}
+
 // RavoRecord defines a Ravo XML record.
 type RavoRecord struct {
-	Klant             bool `xml:"klant"`
-	Sales             bool `xml:"Sales"`
-	AfterSalesService bool `xml:"AfterSalesService"`
-	Productie         bool `xml:"Productie"`
-	Kwaliteit         bool `xml:"Kwaliteit"`
-	Inkoop            bool `xml:"Inkoop"`
-	Engineering       bool `xml:"Engineering"`
+	Klant                                     bool   `xml:"klant"`
+	Sales                                     bool   `xml:"Sales"`
+	AfterSalesService                         bool   `xml:"AfterSalesService"`
+	Productie                                 bool   `xml:"Productie"`
+	Kwaliteit                                 bool   `xml:"Kwaliteit"`
+	Inkoop                                    bool   `xml:"Inkoop"`
+	Engineering                               bool   `xml:"Engineering"`
+	Overige                                   bool   `xml:"Overige"`
+	Overige2                                  bool   `xml:"Overige2"`
+	IngediendDoor                             string `xml:"IngediendDoor"`
+	RWVnummer                                 int64  `xml:"RWVnummer"`
+	DatumIngediend                            string `xml:"DatumIngediend"`
+	VeiligheidsRisico                         bool   `xml:"VeiligheidsRisico"`
+	MilieuSchadeRisico                        bool   `xml:"MilieuSchadeRisico"`
+	Klantklacht                               bool   `xml:"Klantklacht"`
+	WettelijkVereist                          bool   `xml:"WettelijkVereist"`
+	GarantieAanspraak                         bool   `xml:"GarantieAanspraak"`
+	NietConformFunctieEisen                   bool   `xml:"NietConformFunctieEisen"`
+	NietConformKwaliteitEisen                 bool   `xml:"NietConformKwaliteitEisen"`
+	EfficiencyVeranderingMontageMinuten       bool   `xml:"EfficiencyVeranderingMontageMinuten"`
+	KostprijsVerandering                      bool   `xml:"KostprijsVerandering"`
+	ProductStandaardisatie                    bool   `xml:"ProductStandaardisatie"`
+	ProductwijzigingLeverancier               bool   `xml:"ProductwijzigingLeverancier"`
+	ProductwijzigingIntern                    bool   `xml:"ProductwijzigingIntern"`
+	Anders                                    bool   `xml:"Anders"`
+	VeiligheidsrisicoAantal                   RavoBool
+	MilieuschadeRisicoAantal                  RavoBool
+	KlantklachtAantal                         RavoBool
+	WettelijkVereistAantal                    RavoBool
+	GarantieAanspraakAantal                   RavoBool
+	NietConformFunctieEisenAantal             RavoBool
+	NietConformKwaliteitEisenAantal           int64 `xml:"NietConformKwaliteitEisenAantal"`
+	EfficiencyVeranderingMontageMinutenAantal RavoBool
+	KostprijsVeranderingAantal                int64 `xml:"KostprijsVeranderingAantal"`
+	ProductStandaardisatieAantal              RavoBool
+	ProductwijzigingLeverancierAantal         RavoBool
+	ProductwijzigingInternAantal              RavoBool
+	AndersAantal                              RavoBool
+	AndersWat                                 string `xml:"AndersWat"`
+	Probleemstelling                          RavoHTML
 }
 
 // ReadRavoRecord read a Ravo XML record.
